763dd0f7e29c2ed6b658c9d73b0abe48cae4acec
xlbs <928135874@qq.com>
2018-03-15 23:48:36 +0800

    第一次提交

--- SpringMVC/src/com/xlbs/springmvc/controller/ViewControllerTest.java
+++ SpringMVC/src/com/xlbs/springmvc/controller/ViewControllerTest.java
@@ -1,16 +1,31 @@
 package com.xlbs.springmvc.controller;
 
+import com.xlbs.clientservice.daofactory.DAOFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/viewTest")
 public class ViewControllerTest {
 
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("SELECT * FROM `bs_user`");
+        List<Map> list = DAOFactory.getInstance().getQueryInterface().querySqlReturnListMap(sb.toString());
+        System.out.print(list.toString());
+    }
+
     @RequestMapping(value = "/ftlTest")
     public ModelAndView ftlView(){
         ModelAndView mav = new ModelAndView();
+        StringBuffer sb = new StringBuffer();
+        sb.append("SELECT * FROM `bs_user`");
+        List<Map> list = DAOFactory.getInstance().getQueryInterface().querySqlReturnListMap(sb.toString());
+        System.out.print(list.toString());
         mav.setViewName("index.ftl");
         return mav;
     }
